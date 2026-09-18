8e944a0942e160abc737b09e866d09662f62fe04
Jose M Salgado <hola@josemanuelsalgado.com>
2026-09-18 15:23:05 -0700

    updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/welcome">
+            to="/docs/process_designer/intro_to_process_designer">
             Manual for 7.1.0-Beta92-68795
           </Link>
         </div>
@@ -41,3 +41,4 @@ export default function Home() {
     </Layout>
   );
 }
+
